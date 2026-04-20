fc8cf244841d2297032365f32f3c01eaadb833d9
202508915197 <202508915197@alunos.estacio.br>
2026-04-20 20:19:42 +0000

    commit24

--- TechHub/src/script-graficos.js
+++ TechHub/src/script-graficos.js
@@ -8,6 +8,16 @@
 
 // Conexão com o banco de dados
 
+// Comentário
+// Verica em schema.sql se usuario tem permissão para acessar a tabela "pessoas" e se a tabela existe
+// Se a tabela existir, faça uma consulta para selecionar todas as pessoas e imprima os resultados
+
+// script7.js vai vai a verificação e o arquivo schema.sql vai criar a tabela e inserir os dados
+
+// o arquivo math.php vai servir para fazer buscas no banco de dados e retornar os resultados para o script7.js
+
+// Conexão com o banco de dados
+
 // ===== SCRIPT PARA GRÁFICOS COM CHART.JS =====
 
 document.addEventListener('DOMContentLoaded', () => {
